d0cd4c121a77b7e068c423c79d5a0b5490e659e1
CostkaKukru <fifqa87@gmail.com>
2024-09-14 23:43:04 +0200

    adjusting headers, cleaning up code

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { ref } from 'vue';
 
 import { VueperSlides, VueperSlide } from 'vueperslides';
 import 'vueperslides/dist/vueperslides.css';
@@ -53,34 +52,33 @@ const slides = [
 <template>
   <header>
     <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="https://carsspot.com" class="flex items-center">
-                <img src="./assets/Group 255.png" class="mr-3 h-6 sm:h-9" alt="CarsSpot Logo" />
-            </a>
-            <div class="flex items-center lg:order-2">
-                <a href="#" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Zadzwoń do nas</a>
-
-            </div>
+      <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+        <a href="https://carsspot.com" class="flex items-center">
+          <img src="./assets/Group 255.png" class="mr-3" alt="CarsSpot Logo" />
+        </a>
+          <div class="flex items-center lg:order-2">
+            <a href="#" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Zadzwoń do nas</a>
+          </div>
             <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                 <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                     <li>
-                        <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Galeria</a>
+                        <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Galeria zdjęć</a>
                     </li>
                     <li>
-                        <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">FaQ</a>
+                        <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">FaQ</a>
                     </li>
                 </ul>
-            </div>
         </div>
+      </div>
     </nav>
   </header>
 
-<section class="bg-white dark:bg-gray-900">
+  <section>
     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div class="mr-auto place-self-center lg:col-span-7">
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Sprzedajemy samochody<br>z Europy</h1>
-            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Kup komfortowy pojazd, aby każda podróż<br> była wyjątkowym przeżyciem.</p>
-            <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+        <div class="mr-auto place-self-center lg:col-span-6">
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">Sprzedajemy samochody<br>z Europy</h1>
+            <p class="max-w-2xl mb-6 font-light text-neutral-800 lg:mb-8 md:text-lg lg:text-xl">Kup komfortowy pojazd, aby każda podróż<br> była wyjątkowym przeżyciem.</p>
+            <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300">
                 Zobacz zdjęcia
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
@@ -88,22 +86,22 @@ const slides = [
                 Zadzwoń do nas
             </a> 
         </div>
-        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            <img src="./assets/Hero.png" alt="Auta">
+        <div class="lg:mt-0 lg:col-span-6 lg:flex lg:items-end">
+            <img src="./assets/Hero.png" alt="Auta" class="offset-bottom">
         </div>                
     </div>
 </section>
 
-<section class="bg-white dark:bg-gray-900">
+<section>
   <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <div class="max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400">
-            <h5 class="mb-2 font-medium uppercase">Prezentacja firmy</h5>
-          <h2 class="mb-4 text-4xl tracking-tight font-bold text-gray-900 dark:text-white">Zobacz naszą galerię zdjęć</h2>
-          <a href="#" class="inline-flex items-center font-medium text-primary-600 hover:text-primary-800 dark:text-primary-500 dark:hover:text-primary-700">
+            <h3 class="mb-3">Prezentacja firmy</h3>
+          <h2 class="mb-7 tracking-tight text-gray-900 dark:text-white">Zobacz naszą galerię zdjęć</h2>
+          <a href="#" class="inline-flex items-center hover:font-medium text-primary-600 hover:text-primary-800">
               Samochody osobowe
               <svg class="ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
           </a>
-          <a href="#" class="inline-flex items-center font-medium text-primary-600 hover:text-primary-800 dark:text-primary-500 dark:hover:text-primary-700">
+          <a href="#" class="inline-flex items-center font-medium text-primary-600 hover:text-primary-800">
               Samochody dostawcze
               <svg class="ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>  
           </a>
@@ -111,7 +109,7 @@ const slides = [
   </div>
 </section>
 
-    <main class = "container">
+    <main class = "container mx-auto max-w-screen-xl">
         
         <vueper-slides
             class="no-shadow"
@@ -129,17 +127,18 @@ const slides = [
 
     <!--Footer container-->
     <footer
-        class="flex flex-col items-center bg-zinc-50 text-center text-surface dark:bg-neutral-700 dark:text-white lg:text-left">
-        <div class="container p-6">
+        class="flex flex-col items-center bg-neutral-800 text-white text-center text-surface lg:text-left">
+        <div class="container p-6 mx-auto max-w-screen-xl">
             <div class="grid gap-4 lg:grid-cols-2">
                 <div class="mb-6 md:mb-0">
-                <h5 class="mb-2 font-medium uppercase">Footer text</h5>
+                <h5 class="mb-2">Id nostrud enim esse consectetur in eu mollit fugiat aute. Velit nulla commodo.</h5>
 
-                <p class="mb-4">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste
-          atque ea quis molestias. Fugiat pariatur maxime quis culpa
-          corporis vitae repudiandae aliquam voluptatem veniam, est atque
-          cumque eum delectus sint!
+                <p class="mb-4 font">
+                  Mauris varius ipsum mauris, 
+                  rutrum lobortis magna efficitur a. 
+                  Donec egestas, nisl vehicula feugiat 
+                  ornare, diam turpis efficitur mi, ac 
+                  venenatis eros ipsum ac arcu. Vestibulum et  [...]
         </p>
       </div>
 
@@ -169,7 +168,7 @@ const slides = [
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto+Condensed:wght@400;500;700&family=Roboto+Flex:wght@400;500;700&display=swap');
 
-body {
+body{
   font-family: 'Roboto Condensed';
   font-weight: 400;
   font-size: 16px;
@@ -180,12 +179,34 @@ h1 {
   font-size: 76.29px;
 }
 
+h2 {
+  font-family: 'Bebas Neue', sans-serif;
+  font-size: 40px;
+}
+
+h3 {
+  font-family: 'Roboto Condensed';
+  font-weight: 400;
+  font-size: 21.5px;
+}
+
 h5 {
   font-family: 'Bebas Neue', sans-serif;
-  font-size: 76.29px;
+  font-size: 25px;
 }
 
 button, a {
   font-family: 'Roboto Flex';
 }
+
+.original-size {
+    width: 200%;
+    height: auto;
+}
+
+.offset-bottom {
+    position: relative;
+    top: 100px; 
+}
+
 </style>
